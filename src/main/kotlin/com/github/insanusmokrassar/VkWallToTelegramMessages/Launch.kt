79d0999ee66a757bd2319cdd297b4b3e54cc12c1
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-06-11 13:36:47 +0800

    remove get user id mode

--- src/main/kotlin/com/github/insanusmokrassar/VkWallToTelegramMessages/Launch.kt
+++ src/main/kotlin/com/github/insanusmokrassar/VkWallToTelegramMessages/Launch.kt
@@ -9,10 +9,6 @@ import kotlinx.coroutines.experimental.runBlocking
 import java.util.concurrent.TimeUnit
 
 private class LauncherArgumentsParser(parser: ArgParser) {
-    val getUserIdMode by parser.flagging("Enable get user ID mode to know your id for bot. " +
-        "This mode will usefull if you want to receive messages in private messages/group/channel or in channel " +
-        "which have no username.\n\n**** WARNING! ****\n\nPrivate messages with bot require your id, not username!"
-    ).default(false)
     val config_file by parser.positional("File path to config file")
 }
 
@@ -20,20 +16,16 @@ fun main(args: Array<String>) {
     try {
         ArgParser(args).parseInto(::LauncherArgumentsParser).run {
             val config = load(config_file).toObject(Config::class.java)
-            if (getUserIdMode) {
-                getUserIdMode(config.botApiToken)
-            } else {
-                val telegramBotIntegration = TelegramBotIntegration(
-                    config
-                )
-                val vkIntegration = VKIntegration(
-                    config,
-                    telegramBotIntegration.callback,
-                    DB(config.adaptedDbPlace)
-                )
-                runBlocking {
-                    delay(10, TimeUnit.MINUTES)
-                }
+            val telegramBotIntegration = TelegramBotIntegration(
+                config
+            )
+            val vkIntegration = VKIntegration(
+                config,
+                telegramBotIntegration.callback,
+                DB(config.adaptedDbPlace)
+            )
+            runBlocking {
+                delay(10, TimeUnit.MINUTES)
             }
         }
     } catch (e: ShowHelpException) {
